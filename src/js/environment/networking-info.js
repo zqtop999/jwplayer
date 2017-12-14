da9f2b35de82007d7f820ddd6373231b3ac46f6d
karimJWP <karimJWP@github.com>
2017-12-14 15:21:07 -0500

    adds networkingInfo module JW8-946

--- src/js/environment/networking-info.js
+++ src/js/environment/networking-info.js
@@ -1,26 +1,13 @@
-const navigator = window.navigator;
-
-const networkingDefaults = {
-    type: undefined,
-    effectiveType: undefined,
-    downlinkMax: 0,
-    downlink: 0,
-    rtt: 0,
-    saveData: false,
-};
-
-export default function getNetworkInfo(model) {
-    const networkingState = Object.assign({
-        onLine: navigator.onLine,
-        bandwidthEstimate: model.get('bandwidthEstimate')
-    }, networkingDefaults);
-
-    // navigator.connection properties are not enumerable, so copy over default keys
-    const networkInfo = navigator.connection;
-
-    Object.keys(networkingDefaults).forEach(property => {
-        networkingState[property] = networkInfo[property];
-    });
-
-    return networkingState;
+function NetworkingInfo () {
+    this.type = undefined;
+    this.effectiveType = undefined;
+    this.downlinkMax = 0;
+    this.downlink = 0;
+    this.rtt = 0;
+    this.saveData = false;
+    this.onLine = false;
 }
+
+Object.assign(NetworkingInfo.prototype, navigator.connection);
+
+export default NetworkingInfo;
